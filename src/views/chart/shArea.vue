9372cef024e6dd432849962db16adffa2d7f22d7
huangchunlong <huangchunlong@itangquan.com>
2020-10-18 03:01:16 +0800

    区域定位

--- src/views/chart/shArea.vue
+++ src/views/chart/shArea.vue
@@ -1,5 +1,22 @@
 <template>
-  <div id="shArea" :style="{ width: '100%', height: '655px' }" />
+  <div class="shArea" :style="{ width: '100%', height: '655px' }">
+    <div id="shArea" :style="{ width: '100%', height: '655px' }"></div>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+    <span><i></i><i></i><i></i></span>
+  </div>
 </template>
 
 <script>
@@ -30,23 +47,23 @@ export default {
       var dom = document.getElementById("shArea");
       var myChart = this.echarts.init(dom);
       this.echarts.registerMap("ShangHai", geoJson);
-      var data = [{ name: "崇明区", value: 33 }];
-      var geoCoordMap = {
-        崇明区: [121.5637, 31.5383]
-      };
-      var convertData = function(data) {
-        var res = [];
-        for (var i = 0; i < data.length; i++) {
-          var geoCoord = geoCoordMap[data[i].name];
-          if (geoCoord) {
-            res.push({
-              name: data[i].name,
-              value: geoCoord.concat(data[i].value)
-            });
-          }
-        }
-        return res;
-      };
+      // var data = [{ name: "崇明区", value: 33 }];
+      // var geoCoordMap = {
+      //   崇明区: [121.5637, 31.5383]
+      // };
+      // var convertData = function(data) {
+      //   var res = [];
+      //   for (var i = 0; i < data.length; i++) {
+      //     var geoCoord = geoCoordMap[data[i].name];
+      //     if (geoCoord) {
+      //       res.push({
+      //         name: data[i].name,
+      //         value: geoCoord.concat(data[i].value)
+      //       });
+      //     }
+      //   }
+      //   return res;
+      // };
       // 绘制图表
       myChart.setOption({
         title: {
@@ -57,6 +74,7 @@ export default {
         series: [
           {
             name: "",
+            nameProperty: "name",
             type: "map",
             mapType: "ShangHai", // 自定义扩展图表类型
             itemStyle: {
@@ -68,12 +86,15 @@ export default {
               shadowBlur: 5
             },
             coordinateSystem: "bmap",
-            data: convertData(data),
+            data: [{ name: "崇明区", value: 33, tooltip: {} }],
             symbolSize: function(val) {
               return val[2] / 10;
             },
             encode: {
               value: 2
+            },
+            nameMap: {
+              崇明区: "崇明区"
             },
             showEffectOn: "render",
             rippleEffect: {
@@ -88,3 +109,164 @@ export default {
   }
 };
 </script>
+<style scoped>
+.shArea {
+  position: relative;
+}
+.shArea span {
+  position: absolute;
+  color: aliceblue;
+  height: 10px;
+}
+.shArea span::after {
+  content: "";
+  width: 140px;
+  position: absolute;
+  top: -3px;
+  left: 20px;
+}
+.shArea span i {
+  position: absolute;
+  left: 0%;
+  top: 50%;
+  width: 2px;
+  height: 2px;
+  background: aqua;
+  margin-left: -1px;
+  margin-top: -1px;
+  border-radius: 50%;
+  animation: bowen 1.5s ease 0s infinite running;
+}
+.shArea span i:nth-child(2) {
+  animation: bowen 1.5s ease -0.25s infinite running;
+}
+.shArea span i:nth-child(3) {
+  animation: bowen 1.5s ease -0.45s infinite running;
+}
+.shArea span:hover i {
+  -webkit-animation-play-state: running;
+}
+@-webkit-keyframes bowen {
+  0% {
+    width: 1px;
+    height: 1px;
+    opacity: 1;
+  }
+  100% {
+    width: 40px;
+    height: 40px;
+    margin-left: -20px;
+    margin-top: -20px;
+    opacity: 0;
+  }
+}
+.shArea span:nth-child(2)::after {
+  content: "崇明县法院海岛法庭";
+}
+.shArea span:nth-child(3)::after {
+  content: "宝山区人民法院";
+}
+.shArea span:nth-child(4)::after {
+  content: "嘉定区人民法院";
+  left: -140px;
+}
+.shArea span:nth-child(5)::after {
+  content: "静安区人民法院";
+  left: 5px;
+}
+.shArea span:nth-child(6)::after {
+  content: "虹口区人民法院";
+}
+.shArea span:nth-child(7)::after {
+  content: "普陀区人民法院";
+  left: -140px;
+}
+.shArea span:nth-child(8)::after {
+  content: "长宁区人民法院";
+  left: -140px;
+}
+.shArea span:nth-child(9)::after {
+  content: "黄埔区人民法院";
+}
+.shArea span:nth-child(10)::after {
+  content: "青浦区朱家角人民法庭";
+}
+.shArea span:nth-child(11)::after {
+  content: "徐汇区人民法院";
+}
+.shArea span:nth-child(12)::after {
+  content: "浦东新区人民法院";
+}
+.shArea span:nth-child(13)::after {
+  content: "松江区人民法院";
+}
+.shArea span:nth-child(14)::after {
+  content: "闵行区人民法院";
+}
+.shArea span:nth-child(15)::after {
+  content: "奉贤区人民法院";
+}
+.shArea span:nth-child(16)::after {
+  content: "金山区人民法院";
+}
+.shArea span:nth-child(2) {
+  top: 200px;
+  left: 340px;
+}
+.shArea span:nth-child(3) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(4) {
+  top: 280px;
+  left: 215px;
+}
+.shArea span:nth-child(5) {
+  top: 326px;
+  left: 290px;
+}
+.shArea span:nth-child(6) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(7) {
+  top: 330px;
+  left: 273px;
+}
+.shArea span:nth-child(8) {
+  top: 353px;
+  left: 277px;
+}
+.shArea span:nth-child(9) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(10) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(11) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(12) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(13) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(14) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(15) {
+  top: 270px;
+  left: 277px;
+}
+.shArea span:nth-child(16) {
+  top: 270px;
+  left: 277px;
+}
+</style>
